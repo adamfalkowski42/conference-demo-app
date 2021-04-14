c6866e40625912333f138346d50fe8b04b28daaa
falkowa42 <falkowa42@gmail.com>
2021-04-14 07:21:18 -0400

    Solved compile errors

--- src/main/java/com/pluralsight/conferencedemo/config/PersistanceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistanceConfiguration.java
@@ -9,15 +9,15 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistanceConfiguration {
 
-    @Bean
-    public DataSource dataSource(){
+//    @Bean
+//    public DataSource dataSource(){
 //        DataSourceBuilder builder = DataSourceBuilder.create();
 //        builder.url("jdbc:postgresql://localhost:5432/conference_app");
 //        builder.username("postgres");
 //        builder.password("Welcome");
 //        System.out.println("My custom datasource bean has been initialized");
 //        return builder.build();
-        return null;
-
-    }
+//        return null;
+//
+//    }
 }
